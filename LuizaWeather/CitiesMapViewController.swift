0c001e4eeff3856057f0111fbd93e7b8c5276cdf
Kako Botasso <kakodev@gmail.com>
2017-05-31 11:56:13 -0300

    Criando tela de mapa

---
+++ LuizaWeather/CitiesMapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CitiesMapViewController.swift
+//  LuizaWeather
+//
+//  Created by Kako Botasso on 30/05/17.
+//  Copyright © 2017 Kako Botasso. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class CitiesMapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
